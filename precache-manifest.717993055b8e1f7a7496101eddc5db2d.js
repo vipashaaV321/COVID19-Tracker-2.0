868641de23a0a71c87229769540042383079b6ec
vipasha <vips3201v@gmail.com>
2020-08-01 11:49:18 +0530

    Updates

---
+++ precache-manifest.717993055b8e1f7a7496101eddc5db2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421acec8e9cb51a3d23bc9dbe31c04f7",
+    "url": "/COVID19-Tracker-2.0/index.html"
+  },
+  {
+    "revision": "9ac7b993c068ba000305",
+    "url": "/COVID19-Tracker-2.0/static/css/main.00521748.chunk.css"
+  },
+  {
+    "revision": "6e49728c9464a9df7534",
+    "url": "/COVID19-Tracker-2.0/static/js/2.6777111b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19-Tracker-2.0/static/js/2.6777111b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac7b993c068ba000305",
+    "url": "/COVID19-Tracker-2.0/static/js/main.fcf80bdf.chunk.js"
+  },
+  {
+    "revision": "9f02d6891b300769d1fe",
+    "url": "/COVID19-Tracker-2.0/static/js/runtime-main.7b1477e6.js"
+  }
+]);
